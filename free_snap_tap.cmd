c583adc117e1993b4c9c4ff7ec5b96c21d0c2800
Jens Wasielewski <jens.wasielewski@googlemail.com>
2025-07-24 14:50:23 +0200

    Remove obsolete startup scripts and update .gitignore
    Deleted Free_Snap_Tap.vbs and free_snap_tap.cmd as they are no longer needed. Updated .gitignore to exclude .cmd and .vbs files.

--- free_snap_tap.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'free_snap_tap.exe' with Python DLL being found.
-set PATH=c:\users\wasiejen\appdata\local\programs\python\python312;%PATH%
-set PYTHONHOME=C:\Users\Wasiejen\AppData\Local\Programs\Python\Python312
-set NUITKA_PYTHONPATH=C:\Users\Wasiejen\GitHub\Free-Snap-Tap;C:\Users\Wasiejen\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\Wasiejen\AppData\Local\Programs\Python\Python312\Lib;C:\Users\Wasiejen\AppData\Local\Programs\Python\Python312;C:\Users\Wasiejen\GitHub\Free-Snap-Tap\.venv;C:\Users\Wasiejen\GitHub\Free-Snap-Tap\.venv\Lib\site-packages;C:\Users\Wasiejen\GitHub\Free-Snap-Tap\.venv\Lib\site-packages\setuptools\_vendor
-"%~dp0free_snap_tap.exe" %*
